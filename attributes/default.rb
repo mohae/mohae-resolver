5da669ec2fb61a492ee1317434a14a1c121e0752
Count Derp <thecount@moehae.com>
2013-08-18 13:03:11 -0500

    comment out search changes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 # set search to empty since domain isn't set/known yet.
-default['resolver']['search'] = {}
+#default['resolver']['search'] = {}
 
 # use google public dns instaed
 default['resolver']['nameservers'] = ['8.8.8.8', '8.8.4.4']
