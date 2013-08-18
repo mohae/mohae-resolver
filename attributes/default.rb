0e9352d5f1bf062c10b2f83ef062d54b4f2d9295
Count Derp <thecount@moehae.com>
2013-08-18 13:08:27 -0500

    quit worrying about removing search domain line, for now

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,2 @@
-# set search to empty since domain isn't set/known yet.
-normal['resolver']['search'] = {}
-
 # use google public dns instaed
 normal['resolver']['nameservers'] = ['8.8.8.8', '8.8.4.4']
