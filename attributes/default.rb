720677e2bef3da69f3d99fcca8f61867de5dd49e
Count Derp <thecount@moehae.com>
2013-08-18 13:06:22 -0500

    set to normal enable search empty

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 # set search to empty since domain isn't set/known yet.
-#default['resolver']['search'] = {}
+normal['resolver']['search'] = {}
 
 # use google public dns instaed
 normal['resolver']['nameservers'] = ['8.8.8.8', '8.8.4.4']
