55195988cda965f333754c8324076a9ffd784147
Count Derp <thecount@moehae.com>
2013-08-18 12:59:47 -0500

    removed search domain & bump

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,5 @@
-# override default
-override['resolver']['nameservers'] = ['8.8.8.8', '8.8.4.4']
+# set search to empty since domain isn't set/known yet.
+default['resolver']['search'] = {}
+
+# use google public dns instaed
+default['resolver']['nameservers'] = ['8.8.8.8', '8.8.4.4']
